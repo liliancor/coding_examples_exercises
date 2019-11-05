bc1e402fe0ba703ce355081c105ad65ac5595790
liliancor <lilianflavia.cor@hotmail.com>
2019-11-05 20:27:03 -0300

    Add files via upload

---
+++ questionth.py
@@ -0,0 +1,18 @@
+print('Bem vinda ao canal de atendimento da casinha de sape. Pressione: \n1 - conhecer as opcoes de entretenimento\n2 - sair')
+choice = int(input())
+
+#\n quebra linha e end = ' ' finaliza a linha com o que a gente colocar dentro das aspas#
+
+if choice == 1:
+    print('\n\nEscolha uma opcao: \n3 - Ver Bojack')
+    print('4 - Ver big mouth')
+    print('5 - Ver filme')
+    print('6 - Ver greasy anatomy')
+    other_choice = int(input())
+    print('Otima escolha! Deseja adicional de acai pra acompanhar?')
+    third_choice = input()
+    print('Estaremos providenciando seu pedido cliente gatinha safada <3')
+
+if choice == 2:
+    print('Volte sempre <3')
+
